172439cd658f04eaa6df7ee56762261a1ddc40b1
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-04-14 14:57:51 +0200

    Update to manifest version 2

--- zip_include.py
+++ zip_include.py
@@ -1,6 +1,8 @@
 """
 Zips all files listed in manifest["include"]
 """
+from __future__ import annotations
+
 import json
 import os
 import zipfile
@@ -8,15 +10,26 @@ import zipfile
 with open("manifest.json", "r") as f:
     manifest = json.load(f)
 
-to_be_zipped = []
+to_be_zipped: dict[str, list[str]] = {}
 for include in manifest["include"]:
-    if os.path.isdir(include):
-        for path, dirs, files in os.walk(include):
+    location = include["location"]
+    id = include.get("id", "default")
+    if not to_be_zipped.get(id):
+        to_be_zipped[id] = []
+    if os.path.isdir(location):
+        for path, dirs, files in os.walk(location):
             for file in files:
-                to_be_zipped.append(f"{path}/{file}")
+                to_be_zipped[id].append(f"{path}/{file}")
     else:
-        to_be_zipped.append(include)
+        to_be_zipped[id].append(location)
 
-with zipfile.ZipFile("include.zip", "w") as _zip:
-    for file in to_be_zipped:
-        _zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+zips = []
+
+for k, v in to_be_zipped.items():
+    os.makedirs("out", exist_ok=True)
+    with zipfile.ZipFile(f"out/{k}.zip", "w") as _zip:
+        for file in v:
+            _zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+    zips.append(f"out/{k}.zip")
+
+print('\n'.join(zips))
